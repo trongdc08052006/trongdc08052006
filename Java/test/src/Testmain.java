4ea711b0926a10e9385cd0fe40d2d19d47b81f82
trongdc08052006 <trongdc.24itb@vku.udn.vn>
2024-10-15 17:04:01 +0000

    code

---
+++ Java/test/src/Testmain.java
@@ -0,0 +1,18 @@
+public class Testmain {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(1.1);
+        System.out.println(c1);
+        Circle c2 = new Circle(1.0);
+        System.out.println(c2);
+
+        c1.setRadius(2.2);
+        System.out.println(c1);
+        System.out.println("radius is : + c1.setRadius() ");
+        
+
+
+
+
+
+    }
+}
